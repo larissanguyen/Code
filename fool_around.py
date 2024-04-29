dc27b4035f9f004e89acb0c9b6b3ca10146bc2eb
unknown <larissa.h.nguyen@gmail.com>
2024-04-29 17:30:19 -0400

    adding in junk code files for visibility

---
+++ fool_around.py
@@ -0,0 +1,20 @@
+from selenium import webdriver 
+import sys 
+  
+def google_search():  
+    # input from user
+    st = input("Enter string/URL- ")
+    # structuring the input for search
+    search_string = st.replace(' ', '+') 
+  
+    # Assigning the browser variable with chromedriver of google chrome
+    chrome_browser = webdriver.Chrome('chromedriver')
+  
+    # browsing the user input
+    
+    search_input = chrome_browser.get("https://www.google.com/search?q=" +
+                                    search_string + "&start=" + 1)
+
+
+if __name__ == "__main__":
+    google_search()
